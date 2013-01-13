58f6fd401d632f8f508e07379aa81260a3dfb438
Miguel Z <miguelzuniga@gmail.com>
2013-01-12 22:04:21 -0600

    Now the install script downloads amun-client and starts it

--- download/install.sh
+++ download/install.sh
@@ -17,18 +17,11 @@ echo "$TYPE"
 echo "INSTALLING PACKAGES" >> /tmp/script.log
 case "$TYPE" in
    ubuntu)
-     echo "ubuntu" >> /tmp/script.log
-     `sudo apt-get -y update ; sudo apt-get -y install collectd ; sudo apt-get -y install ruby1.9.3 ; sudo gem install stomp`
+     `sudo apt-get -y update ; sudo apt-get -y install collectd ; sudo apt-get -y install ruby1.9.3 ; sudo gem install stomp ; sudo mkdir -p /opt/openescalar/ ; sudo wget -O /opt/openescalar/amun-client.tar.gz http://www.openescalar.org/download/amun-client.tar.gz ; pushd /opt/openescalar ; sudo tar xvfz /opt/openescalar/amun-client.tar.gz ; popd ; sudo ln -s /opt/openescalar/amun-client/bin/amun-client /etc/init.d/ ; sudo /etc/init.d/amun-client start`
      ;;
-   redhat)
-     echo "redhat fork" >> /tmp/script.log
-     `wget -O /root/epel-release-6-8.noarch.rpm http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm ; rpm -ivh /root/epel-release-6-8.noarch.rpm ; yum install -y --nogpgcheck collectd collectd-rrdtool rsyslog rubygem-stomp`
+     `wget -O /root/epel-release-6-8.noarch.rpm http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm ; rpm -ivh /root/epel-release-6-8.noarch.rpm ; yum install -y --nogpgcheck collectd collectd-rrdtool rsyslog rubygem-stomp ; mkdir -p /opt/openescalar/ ; wget -O /opt/openescalar/amun-client.tar.gz http://www.openescalar.org/download/amun-client.tar.gz ; pushd /opt/openescalar/ ; tar xvfz /opt/openescalar/amun-client.tar.gz ; popd ; ln -s /opt/openescalar/amun-client/bin/amun-client /etc/init.d/ ; /etc/init.d/amun-client start`
      ;;
    debian)
-     echo "debian" >> /tmp/script.log
      `apt-get update ; apt-get -y install collectd ; apt-get -y install ruby ; apt-get -y install rubygems ; gem install stomp`
      ;;
 esac
-echo "created openescalar home" >> /tmp/script.log
-mkdir -p /opt/openescalar/
-echo "DONE" >> /tmp/script.log
