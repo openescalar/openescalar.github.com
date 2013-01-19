a7b2cc565efe4ac95e828f8e49cba26eb3e089bf
Miguel Z <miguelzuniga@gmail.com>
2013-01-19 11:05:08 -0600

    removed amunc lient start from install script

--- download/install.sh
+++ download/install.sh
@@ -15,10 +15,10 @@ fi;
 
 case "$TYPE" in
    ubuntu)
-     `sudo apt-get -y update ; sudo apt-get -y install collectd ; sudo apt-get -y install ruby1.9.3 ; sudo gem install stomp ; sudo mkdir -p /opt/openescalar/ ; sudo wget -O /opt/openescalar/amun-client.tar.gz http://www.openescalar.org/download/amun-client.tar.gz ; sudo tar xvfz /opt/openescalar/amun-client.tar.gz -C /opt/openescalar/ ; sudo ln -s /opt/openescalar/amun-client/bin/amun-client /etc/init.d/ ; sudo /etc/init.d/amun-client start`
+     `sudo apt-get -y update ; sudo apt-get -y install collectd ; sudo apt-get -y install ruby1.9.3 ; sudo gem install stomp ; sudo mkdir -p /opt/openescalar/ ; sudo wget -O /opt/openescalar/amun-client.tar.gz http://www.openescalar.org/download/amun-client.tar.gz ; sudo tar xvfz /opt/openescalar/amun-client.tar.gz -C /opt/openescalar/ ; sudo ln -s /opt/openescalar/amun-client/bin/amun-client /etc/init.d/ `
      ;;
    redhat)
-     `wget -O /root/epel-release-6-8.noarch.rpm http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm ; rpm -ivh /root/epel-release-6-8.noarch.rpm ; yum install -y --nogpgcheck collectd collectd-rrdtool rsyslog rubygem-stomp ; mkdir -p /opt/openescalar/ ; wget -O /opt/openescalar/amun-client.tar.gz http://www.openescalar.org/download/amun-client.tar.gz ; tar xvfz /opt/openescalar/amun-client.tar.gz -C /opt/openescalar/ ; ln -s /opt/openescalar/amun-client/bin/amun-client /etc/init.d/ ; /etc/init.d/amun-client start`
+     `wget -O /root/epel-release-6-8.noarch.rpm http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm ; rpm -ivh /root/epel-release-6-8.noarch.rpm ; yum install -y --nogpgcheck collectd collectd-rrdtool rsyslog rubygem-stomp ; mkdir -p /opt/openescalar/ ; wget -O /opt/openescalar/amun-client.tar.gz http://www.openescalar.org/download/amun-client.tar.gz ; tar xvfz /opt/openescalar/amun-client.tar.gz -C /opt/openescalar/ ; ln -s /opt/openescalar/amun-client/bin/amun-client /etc/init.d/ `
 
      ;;
    debian)
